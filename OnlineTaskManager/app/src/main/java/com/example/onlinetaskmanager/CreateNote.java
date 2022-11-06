87aa277c2c5dc1d2abb893185c9a36e5e96a0173
DarwisLau <78061567+DarwisLau@users.noreply.github.com>
2022-11-06 20:16:51 +0800

    XML for main note page and new note page

--- OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/CreateNote.java
+++ OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/CreateNote.java
@@ -7,21 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class CreateNote extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_note);
 
-        ImageView AddNoteButtonMain = findViewById(R.id.AddNoteButton);
-        AddNoteButtonMain.setOnClickListener(new View.OnClickListener() {
+        ImageView BackButtonMain = findViewById(R.id.backButton);
+        BackButtonMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), CreateNote.class);
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
                 view.getContext().startActivity(intent);}
         });
-
     }
 }
--- OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/LoginActivity.java
+++ OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+
 
         setContentView(R.layout.activity_login);
 

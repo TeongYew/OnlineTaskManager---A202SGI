ed65ef0cdf63b929589b589e4f80617bde2b3041
faisalmadd <84001348+faisalmadd@users.noreply.github.com>
2022-11-21 18:26:19 +0800

    Added retrieve user data from FireStore

--- OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/ProfileActivity.java
+++ OnlineTaskManager/app/src/main/java/com/example/onlinetaskmanager/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.onlinetaskmanager;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -19,6 +20,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -29,6 +37,8 @@ public class ProfileActivity extends AppCompatActivity {
     TextView name, email;
     ImageView profilePic;
     Button btnLogout;
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +63,27 @@ public class ProfileActivity extends AppCompatActivity {
             email.setText(personEmail);
             Picasso.get().load(displayPhoto).into(profilePic);
         }
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userID = user.getUid();
+            Uri photoUrl = user.getPhotoUrl();
+
+            db = FirebaseFirestore.getInstance();
+            DocumentReference documentReference = db.collection("users").document(userID);
+            documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                    name.setText(value.getString("username"));
+                    email.setText(value.getString("email"));
+                }
+            });
+
+            if (photoUrl != null){
+                Picasso.get().load(photoUrl).into(profilePic);
+            }
+        }
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
